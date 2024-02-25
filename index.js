6e26213de326dad0f9aa225e5b28321875958e30
zain00abd <zainabflash1998@gmail.com>
2024-02-25 23:38:22 +0200

    was

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express")
 const mongoose = require('mongoose');
 
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 
 
-
+app.get('/', (req,res) =>{
+    res.send("hello zain")
+})
 
 
 
